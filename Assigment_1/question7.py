ec2e97c392c753391698046e9e4a0d24710a8a6d
J.J. Rondon <82114258+Joaron4@users.noreply.github.com>
2026-01-27 13:37:55 -0700

    Added days in calculation of seconds

--- Assigment_1/question7.py
+++ Assigment_1/question7.py
@@ -10,21 +10,23 @@ The function should return a formatted string. If the input is invalid, return a
 message.
 """
 def convert_seconds_to_time(total_seconds):
-   
-    seconds_in_a_day = 86400
-    if total_seconds < 0 or total_seconds >= seconds_in_a_day:
-        return "Enter a number of seconds between 0 and 86399."
-    
     if type(total_seconds) != int and type(total_seconds) != float:
         return "Enter a numeric value for seconds."
+
+    if total_seconds < 0:
+        return "Enter a non-negative number of seconds."
+
+    total_seconds = int(total_seconds)
+    days = total_seconds // 86400
+    remaining_seconds = total_seconds % 86400
     
-    hours = total_seconds // 3600
-    minutes = (total_seconds % 3600) // 60
-    seconds = total_seconds % 60
+    hours = remaining_seconds // 3600
+    minutes = (remaining_seconds % 3600) // 60
+    seconds = remaining_seconds % 60
 
     seconds_until_noon = 43200
 
-    if total_seconds < seconds_until_noon:
+    if remaining_seconds < seconds_until_noon:
         period = "AM"
     else:
         period = "PM"
@@ -34,7 +36,7 @@ def convert_seconds_to_time(total_seconds):
     elif period == "AM" and hours == 0:
         hours = 12
     # 5 17 47 AM
-    hours_string = f"{hours} {minutes} {seconds} {period}"
+    hours_string = f"{days} day(s): {hours} {minutes} {seconds} {period}"
     return hours_string
 
-print(convert_seconds_to_time(50_000))  
+print(convert_seconds_to_time(500_000))  
